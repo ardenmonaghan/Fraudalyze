d15ca37b4bb7fe6082801b53bd2d736f68be6a51
bigriceman0128 <112298940+ardenmonaghan@users.noreply.github.com>
2025-03-02 19:37:53 -0700

    comment changes and documentation

--- Analysis_Scripts/csv_to_sql.py
+++ Analysis_Scripts/csv_to_sql.py
@@ -4,67 +4,45 @@ from sqlalchemy import create_engine, Column, Integer, String, Float, MetaData, 
 
 
 def csv_to_sql(csv_file_path, connection_string, table_name, table_initalization_query):
-    ''' Reads the csv file and puts it into the sql table.'''
+    ''' Reads the csv file and puts it into the sql table.
 
+    Args: csv_file_path: str - The path to the csv file
+    connection_string: str - The connection string to the database
+    table_name: str - The name of the table to be created
+    table_initalization_query: str - The query to initialize the table
+
+    Returns: None
+
+    '''
+
+    # Connecting to the Dataframe from CSV File path
     print(f"Reading csv file from {csv_file_path}")
     df = pd.read_csv(csv_file_path)
 
+    # Connecting to the Database or Creating a new one if it doesn't exist.
     database = sqlite3.connect(connection_string)
     cursor = database.cursor()
 
     cursor.execute(table_initalization_query)
     database.commit()
-
     print(f"Connected to database {connection_string}")
-
-    # If exists = false, if it exists, it will be overwritten.
-    # index - False, dataframes index will not be written as a seperate column
 
     # Removed the unnamed 0 column from the dataframe
     if 'Unnamed: 0' in df.columns:
         df.drop(columns=['Unnamed: 0'], inplace=True)
 
+    ############################################################
+    # Insert Test cases for Valid Dataframe here
+    ############################################################
+
+    # Writing the dataframe to the Database
     df.to_sql(table_name, database, if_exists='append', index=False)
 
-    # Read the data from the database
+    # Test Reading data from the Database
     query = f"SELECT * FROM {table_name}"
     df_from_db = pd.read_sql_query(query, database)
     print(df_from_db.head())
 
+    # Close the Database and Return
     database.close()
-
     print("Successful creation of SQLite DB from Dataframe")
-
-
-def sql_to_csv_postgres(connection_string, table_name, csv_file_path):
-
-    # Define the connection and engine
-    engine = create_engine('sqlite:///my_database.db')
-    metadata = MetaData()
-
-    # Define the table schema with a primary key on 'Cust_ID'
-    ins_l_table = Table('ins_l', metadata,
-                        Column('Cust_ID', String, primary_key=True),
-                        Column('Date', String),
-                        Column('Type', String),
-                        Column('Reward_R', Integer),
-                        Column('Reward_A', Integer),
-                        Column('diff', Integer),
-                        Column('Cov_Limit', Integer),
-                        Column('Income', Integer),
-                        Column('Cov_Income_Ratio', Float),
-                        Column('fraud_flag', Integer)
-                        )
-
-    # Create the table in the database
-    metadata.create_all(engine)
-
-    # Load your data from CSV into a DataFrame
-    df = pd.read_csv('ins_l.csv')
-
-    # Optionally, drop any unwanted columns (e.g., an auto-generated index column)
-    if 'ID' in df.columns:
-        df.drop(columns=['ID'], inplace=True)
-
-    # Insert the data into the pre-created table
-    df.to_sql('ins_l', engine, if_exists='append', index=False)
